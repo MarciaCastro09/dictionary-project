cd1ee41b19491a0a3868c9d3b9eee6c7626b15af
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-08-15 11:58:22 +0100

    Add changed files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo_shecodes from './logo_shecodes.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo_shecodes} className="App-logo img-fluid"
+      alt="logo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
