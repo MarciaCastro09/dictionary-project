b0ce658fb1906b99fed4ef0bb0348cd9ab79f511
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-08-15 23:17:51 +0100

    Add changed files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 
 export default function Dictionary()   {
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response);
+    }
     
     
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${keyword} definition`);
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiURL).then(handleResponse);
+        console.log(apiURL);
     }
+
+    
 
 
     function handleKeywordChange(event) {
