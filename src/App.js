76b7ac2206e6b4ee0a609f75a8f7deb031162b40
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-08-15 22:04:30 +0100

    Add changed files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo_shecodes from './logo_shecodes.png';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header">
       <img src={logo_shecodes} className="App-logo img-fluid"
       alt="logo"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <small>Coded by Márcia Castro</small></footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
