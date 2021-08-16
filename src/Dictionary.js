f6b2aa6cbd978d685ba6fd96d5f45b85526ba58c
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-08-16 18:03:21 +0100

    Add changed files

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 
 export default function Dictionary()   {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState({});
 
     function handleResponse(response) {
-        console.log(response);
+        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
     
     
@@ -33,6 +36,7 @@ export default function Dictionary()   {
     <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
     </form>
+    <Results results={results} />
     </div>
     );
     
