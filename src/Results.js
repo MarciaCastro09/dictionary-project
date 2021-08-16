1ebc80b62f6deff1b9c0cadd04a501e18c19dec3
MarciaCastro09 <81990973+MarciaCastro09@users.noreply.github.com>
2021-08-16 19:10:56 +0100

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Meaning from "./Meaning";
 
-export default function Results(props) {
-  console.log(props.results);
-  if (props.results) {
-    return (
-      <div className="Results">
+export default function Results(props)  {
+    if (props.results) {
+        return (
+        <div className="Results">
         <h2>{props.results.word}</h2>
-        
-      </div>
-    );
-  } else {
+        {props.results.meanings.map(function (meaning, index) {
+                return (
+                <div key={index}> 
+                 <Meaning meaning={meaning} />
+                </div>         
+                );
+            })}
+        </div>
+        );
+} else {
     return null;
-  }
+ }
+
 }
